c8b1d76ba3430885811cd790ad379185983dccc2
marioq <mario300@gmail.com>
2026-01-16 00:00:32 -0300

    chore: Set color models

--- src/client/components/models/Macbook14.jsx
+++ src/client/components/models/Macbook14.jsx
@@ -8,11 +8,27 @@ Source: https://sketchfab.com/3d-models/macbook-pro-m3-16-inch-2024-8e34fc2b3031
 Title: macbook pro M3 16 inch 2024
 */
 
+import { useEffect } from 'react'
 import { useGLTF, useTexture } from '@react-three/drei'
+import {Color} from 'three'
+
+import { noChangeParts } from '../../constants/Three'
+import useMariobookStore from '../../store'
 
 function Macbook14(props) {
-  const { nodes, materials } = useGLTF('/models/macbook-14-transformed.glb')
+  const { color } = useMariobookStore()
+  const { nodes, materials, scene } = useGLTF('/models/macbook-14-transformed.glb')
   const texture = useTexture('/screen.png')
+
+  useEffect(() => {
+    scene.traverse((child) => {
+        if(child.isMesh) {
+            if(!noChangeParts.includes(child.name)) {
+              child.material.color = new Color(color)
+            }
+        }
+    })
+  }, [color, scene])
 
   return (
     <group {...props} dispose={null}>
--- src/client/components/models/Macbook16.jsx
+++ src/client/components/models/Macbook16.jsx
@@ -8,11 +8,28 @@ Source: https://sketchfab.com/3d-models/macbook-pro-m3-16-inch-2024-8e34fc2b3031
 Title: macbook pro M3 16 inch 2024
 */
 
-import React from 'react'
-import { useGLTF } from '@react-three/drei'
+import { useEffect } from 'react'
+import { useGLTF, useTexture } from '@react-three/drei'
+import {Color} from 'three'
+
+import { noChangeParts } from '../../constants/Three'
+import useMariobookStore from '../../store'
 
 function Macbook16(props) {
-  const { nodes, materials } = useGLTF('/models/macbook-16-transformed.glb')
+  const { color } = useMariobookStore()
+  const { nodes, materials, scene } = useGLTF('/models/macbook-16-transformed.glb')
+  const texture = useTexture('/screen.png')
+
+  useEffect(() => {
+    scene.traverse((child) => {
+        if(child.isMesh) {
+            if(!noChangeParts.includes(child.name)) {
+              child.material.color = new Color(color)
+            }
+        }
+    })
+  }, [color, scene])
+
   return (
     <group {...props} dispose={null}>
       <mesh geometry={nodes.Object_10.geometry} material={materials.PaletteMaterial001} rotation={[Math.PI / 2, 0, 0]} />
@@ -32,7 +49,9 @@ function Macbook16(props) {
       <mesh geometry={nodes.Object_82.geometry} material={materials.gMtYExgrEUqPfln} rotation={[Math.PI / 2, 0, 0]} />
       <mesh geometry={nodes.Object_96.geometry} material={materials.PaletteMaterial003} rotation={[Math.PI / 2, 0, 0]} />
       <mesh geometry={nodes.Object_107.geometry} material={materials.JvMFZolVCdpPqjj} rotation={[Math.PI / 2, 0, 0]} />
-      <mesh geometry={nodes.Object_123.geometry} material={materials.sfCQkHOWyrsLmor} rotation={[Math.PI / 2, 0, 0]} />
+      <mesh geometry={nodes.Object_123.geometry} rotation={[Math.PI / 2, 0, 0]} >
+          <meshBasicMaterial map={texture} />
+      </mesh>
       <mesh geometry={nodes.Object_127.geometry} material={materials.ZCDwChwkbBfITSW} rotation={[Math.PI / 2, 0, 0]} />
     </group>
   )
