1faea076a54ba8b4df120cac0ae3f4d5302043e6
marioq <mario300@gmail.com>
2026-01-15 22:28:01 -0300

    chore: Add Macbook glb model

--- src/client/components/Product.tsx
+++ src/client/components/Product.tsx
@@ -1,7 +1,9 @@
 import clsx from "clsx"
 import useMariobookStore from "../store"
-import { Box, OrbitControls } from "@react-three/drei"
+import { OrbitControls } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
+
+import Macbook14 from './models/Macbook14'
 
 function Product() {
     const { color, scale, setColor, setScale } = useMariobookStore()
@@ -43,7 +45,8 @@ function Product() {
             </div>
 
             <Canvas id="canvas" camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100}}>
-                <Box position={[0, 0, 0]} scale={10 * scale} material-color={color} />
+                <ambientLight intensity={1} />
+                <Macbook14 scale={0.06} position={[-1, 0, 0]} />
                 <OrbitControls enableZoom={false} />
             </Canvas>
         </section>
