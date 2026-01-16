7b2cf8e02cbaf90e27d4644423ae49c1d4af99d7
marioq <mario300@gmail.com>
2026-01-16 18:51:43 -0300

    chore: Add showcase section

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Product from './components/Product'
+import Showcase from './components/Showcase'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <Product />
+      <Showcase />
     </main>
   )
 }
